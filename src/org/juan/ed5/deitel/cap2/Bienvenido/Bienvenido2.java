f3e48ebd285aba273277fc70300486b91fcb4c49
gabo32 <j.gabriel.b@live.com.mx>
2017-07-01 18:57:52 -0500

    Capitulo 2, Bienvenido

---
+++ src/org/juan/ed5/deitel/cap2/Bienvenido/Bienvenido2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.juan.ed5.deitel.cap2.Bienvenido;
+
+/**
+ * Imprimir una linea de texto con varias instrucciones
+ * @author JuanVaio
+ */
+public class Bienvenido2 {
+    
+    //el metodo main empieza la ejecucion de la aplicacion en java
+    public static void main(String[] args) {
+        System.out.print("Bienvenido ");
+        System.out.println(" a la prigramacion de Java!");
+    }//fin del metodo main
+    
+}//fin de la clase bienvenido1
