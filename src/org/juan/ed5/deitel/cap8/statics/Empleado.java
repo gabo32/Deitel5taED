60fbe08b3ac509228580c18f53a8d9bbe00fbea9
gabo32 <j.gabriel.b@live.com.mx>
2017-08-13 01:03:55 -0500

    Paquetes y estaticos

---
+++ src/org/juan/ed5/deitel/cap8/statics/Empleado.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.juan.ed5.deitel.cap8.statics;
+
+/**
+ * Declaracion de clase empleado
+ * @author JuanVaio
+ */
+public class Empleado {
+    private String primerNombre;
+    private String apellidoPaterno;
+    private static int cuenta = 0;
+    
+    public Empleado(String nombre, String apellido){
+        primerNombre = nombre;
+        apellidoPaterno = apellido;
+        
+        ++cuenta;
+        System.out.println("Constreuctor de empleado "+primerNombre  +" "+ apellidoPaterno);
+    }
+    
+    protected void finalize(){
+        --cuenta;
+        System.out.println("Finalizador de empleado "+primerNombre+" "+apellidoPaterno+";cuenta "+cuenta);
+    }
+    
+    public String obtenerPrimerNombre(){
+        return primerNombre;
+    }
+    
+    public String obtenerApellidoPaterno(){
+        return apellidoPaterno;
+    }
+    
+    public static int obtenerCuenta(){
+        return cuenta;
+    }
+}
